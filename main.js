be48e98f55cbbdc4755e4750aa915ea265f728a3
ZennithYattare <luis.espera.0758@gmail.com>
2023-04-06 05:12:56 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import { scatterPlot } from "./scatterPlot";
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <div class="bg-[#242424] flex h-screen w-screen place-items-center">
+    <div class="min-w-[960px] w-[960px] h-[650px] border-4 border-t-slate-200 border-l-slate-200 border-r-[#808080] border-b-[#808080] bg-[#c0c0c0] shadow-[5px_5px_5px_black] mx-auto p-4">
+      <div id="title" class="text-4xl mx-auto w-max">Professional Bicycle Racing Doping Allegations</div>
+      <div id="scatter-plot" class="m-0 p-0"></div>
+    </div>
+  </div>
+`;
+
+scatterPlot(document.querySelector("#scatter-plot"));
---
+++ scatterPlot.js
@@ -0,0 +1,145 @@
+/** @format */
+
+export function scatterPlot() {
+	const width = 920;
+	const height = 570;
+
+	const legend = d3
+		.select("#scatter-plot")
+		.append("g")
+		.attr("id", "legend")
+		.style("opacity", 0.9)
+		.style("position", "absolute")
+		.style("left", "850px")
+		.style("top", "260px")
+		.style("width", "210px")
+		.style("background-color", "white")
+		.style("border", "1px solid black")
+		.style("padding", "6px");
+
+	const tooltip = d3
+		.select("#scatter-plot")
+		.append("div")
+		.attr("id", "tooltip")
+		.style("opacity", 0);
+
+	const svg = d3
+		.select("#scatter-plot")
+		.append("svg")
+		.attr("width", width)
+		.attr("height", height + 10);
+
+	let dataArray = [];
+
+	fetch(
+		"https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
+	)
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			dataArray = data;
+			console.log(dataArray[0]);
+			const xScale = d3
+				.scaleLinear()
+				.domain([
+					d3.min(dataArray, (d) => d.Year - 1),
+					d3.max(dataArray, (d) => d.Year + 1),
+				])
+				.range([0, width - 50]);
+
+			const yScale = d3
+				.scaleTime()
+				.domain(d3.extent(dataArray, (d) => new Date(d.Seconds * 1000)))
+				.range([0, height - 16]);
+
+			const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
+			const yAxis = d3
+				.axisLeft(yScale)
+				.tickFormat(d3.timeFormat("%M:%S"));
+
+			svg.append("g")
+				.call(xAxis)
+				.attr("id", "x-axis")
+				.attr("transform", "translate(35, 554)");
+
+			svg.append("g")
+				.call(yAxis)
+				.attr("id", "y-axis")
+				.attr("transform", "translate(35, 0)");
+
+			svg.selectAll("circle")
+				.data(dataArray)
+				.enter()
+				.append("circle")
+				.attr("cx", (d, i) => xScale(d.Year) + 35)
+				.attr("cy", (d, i) => yScale(new Date(d.Seconds * 1000)))
+				.attr("r", 5)
+				.attr("class", "dot")
+				.attr("data-xvalue", (d, i) => d.Year)
+				.attr("data-yvalue", (d, i) => new Date(d.Seconds * 1000))
+				.style("opacity", 0.6)
+				.style("fill", (d, i) => {
+					if (d.Doping === "") {
+						return "blue";
+					} else {
+						return "red";
+					}
+				})
+				.on("mouseover", (event, d) => {
+					tooltip.style("opacity", 0.9);
+					tooltip.attr("data-year", d.Year);
+					tooltip.html(
+						d.Nationality +
+							": " +
+							d.Name +
+							"<br/>" +
+							"Year: " +
+							d.Year +
+							", Time: " +
+							d.Time +
+							(d.Doping ? "<br/><br/>" + d.Doping : "")
+					);
+
+					tooltip.style("left", event.pageX + "px");
+					tooltip.style("top", event.pageY - 20 + "px");
+				})
+				.on("mouseout", function (d) {
+					tooltip.transition().duration(200).style("opacity", 0);
+				});
+
+			legend
+				.append("text")
+				.text("No doping allegations")
+				.style("font-size", "12px");
+
+			legend
+				.append("svg")
+				.attr("id", "svgLegend")
+				.attr("width", 18)
+				.attr("height", 18)
+				.append("rect")
+				.attr("width", 18)
+				.attr("height", 18)
+				.style("opacity", 0.9)
+				.style("fill", "blue");
+
+			legend.append("br");
+
+			legend
+				.append("text")
+				.text("Riders with doping allegations")
+				.style("font-size", "12px");
+
+			legend
+				.append("svg")
+				.attr("id", "svgLegend")
+				.attr("width", 18)
+				.attr("height", 18)
+				.append("rect")
+				.attr("width", 18)
+				.attr("height", 18)
+				.style("opacity", 0.9)
+				.style("fill", "red");
+		});
+}
