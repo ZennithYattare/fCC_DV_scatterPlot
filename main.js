33bef2593c7896314d0758e2006feacd21996ec5
ZennithYattare <luis.espera.0758@gmail.com>
2023-04-09 02:05:26 +0800

    added heading

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ import "./style.css";
 document.querySelector("#app").innerHTML = `
   <div class="bg-[#242424] flex h-screen w-screen place-items-center">
     <div class="min-w-[960px] w-[960px] h-[650px] border-4 border-t-slate-200 border-l-slate-200 border-r-[#808080] border-b-[#808080] bg-[#c0c0c0] shadow-[5px_5px_5px_black] mx-auto p-4">
-      <div id="title" class="text-4xl mx-auto w-max">Professional Bicycle Racing Doping Allegations</div>
+    <heading>
+      <h1 id="title" class="text-4xl mx-auto w-max">Professional Bicycle Racing Doping Allegations</h1>
+    </heading>
       <div id="scatter-plot" class="m-0 p-0"></div>
     </div>
   </div>
